95db1776525a95eca2d36cbd8211a7a78590e0b4
Dans Bootcamp <dansbootcamp@AmbersImac.home>
2019-10-28 13:43:57 -0400

    Second commit of the new friend finder app

---
+++ README.md
@@ -0,0 +1,4 @@
+# DMfriendfinder
+SUM1 is the ultimate matchmaking app.  We can find you that one very special SUM1 to sail off into the sunset with.  Based on our proven survey of your personality type we will find your matches.  Don't waste another loveless minute of your life, jump right in and take the survey to find love today!!!!
+
+DISCLAIMER: SUM1 is not responsible for any personal interactions between our clients and cannot be held responsible for any legal damages or personal harm incurred during those personal interactions.  Other than that have fun and fall in love!!!!!
---
+++ data/friends.js
@@ -0,0 +1,116 @@
+var friends = [
+
+    {
+        name:"Ahmed",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores:[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+
+      {
+        name:"Helen",
+        photo:"https://i.pinimg.com/originals/93/3f/f7/933ff7f41554403422c3da8bf0bbf654.jpg",
+        scores:[
+           5,2,3,2,4,1,4,1,5,2
+          ]
+      },
+
+      {
+        name:"Rumple foreskin",
+        photo:"https://allthatsinteresting.com/thumb/800.422.https://allthatsinteresting.com/wordpress/wp-content/uploads/2014/06/face-experiment-france.jpg",
+        scores:[
+           5,3,5,4,5,1,5,3,5,2
+          ]
+      },
+
+      {
+        name:"Mayahrea",
+        photo:"https://i.kinja-img.com/gawker-media/image/upload/s--bZDWbWAz--/c_scale,f_auto,fl_progressive,q_80,w_800/pyvljus5onl0b1ulqys9.jpg",
+        scores:[
+           2,4,4,2,4,1,1,1,3,4
+          ]
+      },
+
+      {
+        name:"Peter Old Tool",
+        photo:"https://www.davidphenry.com/other/paris/Ugly-man-Pompidou-Center-6668X.jpg",
+        scores:[
+           2,2,2,4,3,5,3,4,1,3
+          ]
+      },
+
+      {
+        name:"Esther Roids",
+        photo:"https://i.dailymail.co.uk/i/pix/2016/03/11/09/3214AB2C00000578-3487076-Breaking_records_Janice_Lorraine_is_Australia_and_perhaps_the_wo-a-3_1457687968621.jpg",
+        scores:[
+           5,3,4,5,2,2,4,2,5,3
+          ]
+      },
+
+      {
+        name:"Bill and Ted",
+        photo:"https://img.huffingtonpost.com/asset/55f09e4c14000077002e53fd.jpeg?ops=1778_1000",
+        scores:[
+           3,2,2,5,5,1,4,2,4,1
+          ]
+      },
+
+      {
+        name:"Ellen Degenerate",
+        photo:"https://cdn.ebaumsworld.com/mediaFiles/picture/2298480/83252072.jpg",
+        scores:[
+           4,3,5,1,1,3,2,4,1,4
+          ]
+      },
+
+      {
+        name:"Al Abastard",
+        photo:"https://www.askideas.com/media/22/Funny-Half-Shaved-Man-Picture.jpg",
+        scores:[
+           3,3,3,3,4,4,1,1,1,4
+          ]
+      },
+
+      {
+        name:"Sally Babychest",
+        photo:"http://4.bp.blogspot.com/_2SUoaYTC9DU/TCcA1cFlNzI/AAAAAAAAA7k/IgeKFU4zvHY/s1600/najg1.jpg",
+        scores:[
+           4,2,3,2,3,5,1,1,1,3
+          ]
+      },
+
+      {
+        name:"Reggie Stirredoffender",
+        photo:"http://i.dailymail.co.uk/i/pix/2012/09/27/article-2209422-153B2215000005DC-478_468x560.jpg",
+        scores:[
+           4,2,3,1,3,1,4,2,4,5
+          ]
+      },
+
+      {
+        name:"Olive Tostalkew",
+        photo:"https://thewondrous.com/wp-content/uploads/2010/03/weird-haircuts_4a5fa5feca261.jpg",
+        scores:[
+           5,2,3,1,5,2,2,3,1,4
+          ]
+      },
+
+      {
+        name:"Eileen Todaleft",
+        photo:"https://i1.wp.com/metro.co.uk/wp-content/uploads/2017/08/501879193.jpg?quality=90&strip=all&zoom=1&resize=644%2C482&ssl=1",
+        scores:[
+           1,3,2,4,4,2,3,1,3,4
+          ]
+      }
+];
+module.exports = "friends";
